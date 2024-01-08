d08c4942baa1c275ec3e7b9521e3154da4375a0d
WhiteBearV <kusunzaza096@gmail.com>
2024-01-08 20:58:58 +0700

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -1,121 +1,8 @@
 import os
-import keyboard
 import time
-class pos:
-    def __init__(self, y, x) -> None:
-        self.y = y
-        self.x = x
+import sys 
 
-    def __str__(self):
-        return f"({self.y}, {self.x})"
-
-class maze:
-    def __init__(self) -> None:
-        self.maze = [
-                    ["X", "X", "X", "X", "X", "X", "X"],
-                    ["X", " ", " ", " ", "X", " ", "X"],
-                    ["X", " ", "X", " ", "X", " ", " "],
-                    ["X", " ", "X", " ", "X", " ", "X"],
-                    ["X", " ", "X", " ", " ", " ", "X"],
-                    ["X", " ", "X", "X", "X", "X", "X"],
-                    ]
-        self.ply = pos(5, 1)
-        self.end = pos(2, 6)
-        self.maze[self.ply.y][self.ply.x] = "P"
-        self.maze[self.end.y][self.end.x] = "E"
-        self.stack = Stack()
-    
-    def isInBound(self, y, x):
-        if y>=0 and x>=0 and y<len(self.maze) and x<len(self.maze[0]):
-            return True
-        else:
-            return False
-    
-    def print(self):
-        os.system("cls")
-        print("\n\n\n")
-        for row in self.maze:
-            for col in row:
-                print(col," ", end="")
-            print("")
-        print("\n\n\n")
-    
-    def printEND(self):
-        os.system("cls")
-        print("\n\n\n")
-        print(">>>>> Congraturation!!! <<<<<")
-        print("\n\n\n")
-        keyboard.wait("")
-
-    def move_up(self):
-        next_move = pos(self.ply.y - 1, self.ply.x)
-        if self.isInBound(next_move.y, next_move.x):
-            point = self.stack.push(self.ply)
-            if self.maze[next_move.y][next_move.x] == " ":
-                self.maze[self.ply.y][self.ply.x] = " "
-                self.maze[next_move.y][next_move.x] = "P"
-
-                self.ply = next_move
-                time.sleep(0.25)
-            if self.maze[next_move.y][next_move.x] == "E":
-                self.printEND()
-                return False
-        return True , point
-    
-    def move_down(self):
-        next_move = pos(self.ply.y + 1, self.ply.x)
-        if self.isInBound(next_move.y, next_move.x):
-            point =self.stack.push(self.ply)
-            if self.maze[next_move.y][next_move.x] == " ":
-                self.maze[self.ply.y][self.ply.x] = " "
-                self.maze[next_move.y][next_move.x] = "P"
-
-                self.ply = next_move
-                time.sleep(0.25)
-            if self.maze[next_move.y][next_move.x] == "E":
-                self.printEND()
-                return False
-        return True , point
-
-    def move_left(self):
-        next_move = pos(self.ply.y , self.ply.x - 1)
-        if self.isInBound(next_move.y, next_move.x):
-            point =self.stack.push(self.ply)
-            if self.maze[next_move.y][next_move.x] == " ":
-                self.maze[self.ply.y][self.ply.x] = " "
-                self.maze[next_move.y][next_move.x] = "P"
-            
-                self.ply = next_move
-                time.sleep(0.25)
-            if self.maze[next_move.y][next_move.x] == "E":
-                self.printEND()
-                return False
-        return True , point
-    
-    def move_right(self):
-        next_move = pos(self.ply.y, self.ply.x+1)
-        if self.isInBound(next_move.y, next_move.x):
-            point =self.stack.push(self.ply)
-            if self.maze[next_move.y][next_move.x] == " ":
-                self.maze[self.ply.y][self.ply.x] = " "
-                self.maze[next_move.y][next_move.x] = "P"
-            
-                self.ply = next_move
-                time.sleep(0.25)
-            if self.maze[next_move.y][next_move.x] == "E":
-                self.printEND()
-                return False
-        return True , point
-    
-    def undo_move(self):
-        if not self.stack.isEmpty():
-            Stack.push(prev_pos)
-            prev_pos = self.stack.pop()
-            self.ply = prev_pos
-            self.maze[prev_pos.y][prev_pos.x] =   Stack.__len__.points
-            return
-        
-
+mytime = 0.25
 class Stack:
 
     def __init__(self):
@@ -148,32 +35,162 @@ class _StackNode:
         self.item = item
         self.next = link
 
+class maze:
+ 
+    def __init__(self) -> None:
+        self.maze = [
+                    ["X", "X", "X", "X", "X", "X", "X"],
+                    ["X", " ", "X", " ", "X", " ", "X"],
+                    ["X", " ", "X", " ", "X", " ", " "],
+                    ["X", " ", "X", " ", "X", " ", "X"],
+                    ["X", " ", " ", " ", " ", " ", "X"],
+                    ["X", " ", "X", "X", "X", "X", "X"],
+                    ]
+        self.ply = pos(5, 1)
+        self.end = pos(2, 6)
+        self.maze[self.ply.y][self.ply.x] = "P"
+        self.maze[self.end.y][self.end.x] = "E"
+    
+    def isInBound(self, y, x):
+        if y>=0 and x>=0 and y<len(self.maze) and x<len(self.maze[0]):
+            return True
+        else:
+            return False
+    
+    def print(self):
+        os.system("cls")
+        print("\n\n\n")
+        for row in self.maze:
+            for col in row:
+                print(col," ", end="")
+            print("")
+        print("\n\n\n")
+    
+    def printEND(self):
+        os.system("cls")
+        print("\n\n\n")
+        print(">>>>> Congraturation!!! <<<<<")
+        print("\n\n\n")
+        sys.exit()
 
+
+    def move_up(self):
+        next_move = pos(self.ply.y-1, self.ply.x)
+        if self.isInBound(next_move.y,next_move.x):
+            if self.maze[next_move.y][next_move.x] == " ":
+                self.maze[self.ply.y][self.ply.x] = " "
+                self.maze[next_move.y][next_move.x] = "P"
+                self.ply = next_move
+                time.sleep(mytime)
+            if self.maze[next_move.y][next_move.x] == "E":
+                self.printEND()
+                return False
+        return True
+    
+    def move_down(self):
+        next_move = pos(self.ply.y + 1, self.ply.x)
+        if self.isInBound(next_move.y, next_move.x):
+            if self.maze[next_move.y][next_move.x] == (" "):
+                self.maze[self.ply.y][self.ply.x] = " "
+                self.maze[next_move.y][next_move.x] = "P"
+                self.ply = next_move
+                time.sleep(mytime)
+            if self.maze[next_move.y][next_move.x] == "E":
+                self.printEND()
+                return False
+        return True
+
+    def move_left(self):
+        next_move = pos(self.ply.y, self.ply.x-1)
+        if self.isInBound(next_move.y,next_move.x):
+            if self.maze[next_move.y][next_move.x] == " ":
+                self.maze[self.ply.y][self.ply.x] = " "
+                self.maze[next_move.y][next_move.x] = "P"
+                self.ply = next_move
+                time.sleep(mytime)
+            if self.maze[next_move.y][next_move.x] == "E":
+                self.printEND()
+                return False
+        return True
+
+    def move_right(self):
+        next_move = pos(self.ply.y, self.ply.x+1)
+        if self.isInBound(next_move.y,next_move.x):
+            if self.maze[next_move.y][next_move.x] == " ":
+                self.maze[self.ply.y][self.ply.x] = " "
+                self.maze[next_move.y][next_move.x] = "P"
+                self.ply = next_move
+                time.sleep(mytime)
+            if self.maze[next_move.y][next_move.x] == "E":
+                self.printEND()
+                return False
+        return True
+        
+class pos:
+    def __init__(self) -> None:
+        self.y = None
+        self.x = None
+    
+    def __init__(self, y, x) -> None:
+        self.y = y
+        self.x = x
+
+    def __str__ (self) -> str:
+        return f"Pos(x={self.y}, y={self.x})"
+    
 if __name__ == '__main__':
     m = maze()
     m.print()
+    stack = Stack()
+    visited = set()
+
+    print("START")
 
     while True:
-        if keyboard.is_pressed("q"):
-            print("Quit Program")
-            break
-        if keyboard.is_pressed("w"):
-            if m.move_up():
-                p = m.move_up
+        current_pos = (m.ply.y, m.ply.x)
+        m.ply = pos(current_pos[0],current_pos[1])
+        if current_pos not in visited:
+            visited.add(current_pos)
+            
+            if m.maze[m.ply.y - 1][m.ply.x] != "X" and (m.ply.y - 1, m.ply.x) not in visited:
+                m.move_up()
                 m.print()
-        if keyboard.is_pressed("s"):
-            if m.move_down():
-                p = m.move_down
+                stack.push(current_pos)
+                print("MOVE_UP")    
+            #    input()
+
+            elif m.maze[m.ply.y + 1][m.ply.x] != "X" and (m.ply.y + 1, m.ply.x) not in visited:
+                m.move_down()
                 m.print()
-        if keyboard.is_pressed("a"):
-            if m.move_left():
-                p = m.move_left
+                stack.push(current_pos)
+                print("MOVE_DOWN")
+            #    input()
+
+            elif m.maze[m.ply.y][m.ply.x + 1] != "X" and (m.ply.y, m.ply.x + 1) not in visited:
+                m.move_right()
                 m.print()
-        if keyboard.is_pressed("d"):
-            if m.move_right():
-                p = m.move_right
+                stack.push(current_pos)
+                print("MOVE_RIGHT")
+            #    input()
+
+            elif m.maze[m.ply.y][m.ply.x - 1] != "X" and (m.ply.y, m.ply.x - 1) not in visited:
+                m.move_left()
                 m.print()
-        if keyboard.is_pressed("z"): 
-            m.undo_move()
-            m.print()
-            break
+                stack.push(current_pos)
+                print("MOVE_LEFT")
+              #  input()
+
+            else:
+
+                Deadend = True
+
+                while not stack.isEmpty() and Deadend == True : # and m.maze[m.ply.y][m.ply.x] == "P" :
+                    lastmove = stack.pop()
+                    visited.remove(lastmove)
+                    m.maze[current_pos[0]][current_pos[1]] = "D"
+                    m.ply = pos(lastmove[0],lastmove[1])
+                    m.maze[m.ply.y][m.ply.x] = "P"
+                    m.print()
+                    print('Nope No Pass')
+                    Deadend = False
+
